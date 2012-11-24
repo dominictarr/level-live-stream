8bff218f89cecca56a9c397fe8122d638a5a9399
Dominic Tarr <dominic.tarr@gmail.com>
2012-11-23 21:46:23 -0800

    only listen to one close event

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var through = require('through')
 module.exports = function (db) {
 
   if(db.createLiveStream) return db
-
   require('level-hooks')()(db)
 
   db.liveStream = 
   db.createLiveStream = 
   function (opts) {
+    var start = opts.start, end = opts.end, onPost
     opts = opts || {}
-    var start = opts.start, end = opts.end
+
     var ts = through(function (data) {
         this.queue(data)
       }, function() {
@@ -19,18 +19,16 @@ module.exports = function (db) {
 
     ts.range = opts
 
-    function onPost (ch) {
+    db.on('hooks:post', onPost = function (ch) {
       var key = ch.key.toString()
       if((!start || start <= key ) && (!end || key <= end))
         ts.queue(ch)
-    }
-
-    db.on('hooks:post', onPost)
+    })
 
     var rs = this.readStream(opts)
     rs.pipe(ts, {end: false})
 
-    ts.on('close', function () {
+    ts.once('close', function () {
       db.removeListener('hooks:post', onPost)
       rs.destroy()
     })
