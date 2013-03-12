b5f656f575419d89053714a448357f61b52ae37e
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-13 10:56:01 +1300

    rewrite for level-sublevel

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
 var through = require('through')
 
-module.exports = function (db) {
+module.exports = function (db, opts) {
 
-  if(db.createLiveStream) return db
-  require('level-hooks')()(db)
+  var start = opts.start, end = opts.end, onPost
+  opts = opts || {}
 
-  db.liveStream = 
-  db.createLiveStream = 
-  function (opts) {
-    var start = opts.start, end = opts.end, onPost
-    opts = opts || {}
-
-    if(opts.tail === false) {
-      return this.readStream(opts)
-    }
-
-    var ts = through()
-
-    ts.range = opts
-
-    db.on('hooks:post', onPost = function (ch) {
-      var key = ch.key.toString()
-      if((!start || start <= key ) && (!end || key <= end))
-        ts.queue(ch)
-    })
-
-    var rs = this.readStream(opts)
-    rs.pipe(ts, {end: false})
-
-    rs.once('end', function () {
-      ts.emit('sync')
-    })
-
-    ts.once('close', function () {
-      db.removeListener('hooks:post', onPost)
-      rs.destroy()
-    })
-
-    return ts
+  if(opts.tail === false) {
+    return this.readStream(opts)
   }
+
+  var ts = through()
+
+  ts.range = opts
+
+  var removeHook = db.post(opts, function (ch) {
+    ts.queue(ch)
+  })
+
+  var rs = this.readStream(opts)
+  rs.pipe(ts, {end: false})
+
+  rs.once('end', function () {
+    ts.emit('sync')
+  })
+
+  ts.once('close', function () {
+    removeHook()
+    rs.destroy()
+  })
+
+  return ts
 }
