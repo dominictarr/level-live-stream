c0c64780b83e70dbee5aacc5eb2a2b9836082a78
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-14 00:10:05 +0100

    test can just stream live updates

--- test/index.js
+++ test/index.js
@@ -7,6 +7,10 @@ LiveStream(db, {tail: true}).on('data', function (data) {
   db.get(data.key, console.log)
 })
 
+LiveStream(db, {old: false}).on('data', function (data) {
+  console.log('new:', data.key)
+})
+
 setInterval(function () {
   db.put(Math.random().toString(), new Date(), console.log)
 }, 1000)
