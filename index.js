918268d6d68bf7047974e1c411b0b56fed070752
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-13 22:30:55 +0100

    fix all the errors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var through = require('through')
+var pull = require('pull-level')
+var toStream = require('pull-stream-to-stream')
 
 var liveStream = module.exports = function (db, opts) {
   var ts
   opts = opts || {}
+  opts.tail = opts.tail !== false
   opts.onSync = function () {
     ts.emit('sync')
-  }  
-  return ts = toPull(pull.live(db, opts))
+  }
+  return ts = toStream(null, pull.read(db, opts))
 }
 
 module.exports.install = function (db) {
