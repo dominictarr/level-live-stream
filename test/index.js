dfd19b172af096c6f786d5af1a03641c9bd599d1
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-25 08:52:26 -0500

    test deletes

--- test/index.js
+++ test/index.js
@@ -6,19 +6,37 @@ var assert = require('assert')
 var i = 10
 var j = 10
 var k = 10
- 
+
 LiveStream(db, {tail: true}).on('data', function (data) {
-  assert.equal(data.key, j--)
+  console.log(data)
+  if(data.type === 'put')
+    assert.equal(data.key, j--)
 })
 
 LiveStream(db, {old: false}).on('data', function (data) {
-  assert.equal(data.key, k--)
+  if(data.type === 'put')
+    assert.equal(data.key, k--)
 })
 
+var a = []
 var int = setInterval(function () {
-  db.put(i+'', new Date(), function(err) {
-    assert(err == undefined)
-  })
-  if(--i) return
-  clearInterval(int)
+  var key = i + ''
+
+  if(Math.random() < 0.2 && a.length) {
+    var r = ~~(Math.random()*a.length)
+    key = a[r]
+    a.slice(r, 1)
+    db.del(key, function (err) {
+      assert(err == undefined)
+    })
+  }
+  else {
+    a.push(key)
+    db.put(key, new Date(), function(err) {
+      assert(err == undefined)
+    })
+    if(--i) return
+    clearInterval(int)
+  }
+
 }, 100)
