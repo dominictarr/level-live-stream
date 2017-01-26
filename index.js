2af42a93569ff5f9dcc38c92ce07e01b2629ace3
Kaelig <kaelig@deloumeau.fr>
2017-01-25 23:22:20 -0800

    Upgrade to .live instead of .tail - in a backwards compatible manner

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ var toStream = require('pull-stream-to-stream')
 var liveStream = module.exports = function (db, opts) {
   var ts
   opts = opts || {}
+
+  if(opts.tail) {
+    console.error('level-live-stream: .tail option is deprecated. use .live instead')
+  }
   opts.tail = opts.tail !== false
+
+  // opts.tail is deprecated, using opts.live instead
+  if(opts.tail) {
+    opts.live = opts.tail
+  } else {
+    opts.live = opts.live !== false
+  }
+
+  delete opts.tail
+
   if(opts.old === false)
     return toStream(null, pull.live(db, opts))
 
